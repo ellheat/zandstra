c8995418168ab2084c6de4547ffaf967aeef59f0
ellheat <patryk.poleszczuk@gmail.com>
2015-02-24 22:52:13 +0100

    version 0.1.1

--- cechy3.php
+++ cechy3.php
@@ -1,14 +1,5 @@
 <?php
-
-trait TaxTools {
-	function calculateTax($price) {
-		return 222;
-	}
-}
-
 trait PriceUtilities {
-	private $taxrate = 17;
-	
 	function calculateTax($price){
 		return (($this->taxrate/100) * $price);
 	}
@@ -19,15 +10,11 @@ abstract class Service {
 }
 
 class UtilityService extends Service {
-	use PriceUtilities, TaxTools {
-		TaxTools::calculateTax insteadof PriceUtilities;
-		PriceUtilities::calculateTax as basicTax;
-	}
+	public $taxrate = 17;
+	use PriceUtilities;
 }
 
 $u = new UtilityService();
 print $u->calculateTax(100).'<br>';
-print $u->basicTax(100).'<br>';
-
 
 ?>
